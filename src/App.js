863d7ec28773416eeaf8445548fdc10926893c19
Praneeth <kommusaipraneeth@gmail.com>
2023-05-03 22:17:08 +0530

    updated App

--- src/App.js
+++ src/App.js
@@ -8,11 +8,12 @@ import UseState1 from "./components/useState/UseState1";
 import UseState2 from "./components/useState/UseState2";
 import UseState3 from "./components/useState/UseState3";
 import LoginForm from "./components/useState/LoginForm";
+import RegistrationForm from "./components/useState/RegistrationForm";
 
 const App = () => {
   return (
     <div>
-      <LoginForm />
+      <RegistrationForm />
     </div>
   );
 };
